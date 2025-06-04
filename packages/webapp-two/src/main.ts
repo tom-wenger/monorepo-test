1e8f0284e0e01db80bf23d3571b0d40ddb251990
Tom Wenger <tom.wenger@lambda-it.ch>
2025-06-04 17:02:21 +0200

    add stencil

--- packages/webapp-two/src/main.ts
+++ packages/webapp-two/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import {hello} from "@one"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
